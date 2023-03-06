ecfebd16b3d69869b32a29f625abb9c2657be1d2
Vini5577 <ugopessoa@gmail.com>
2023-03-06 18:59:32 -0300

    apagando imports inutilizados

--- src/main/java/com/flexpag/paymentscheduler/entilty/Pagamento.java
+++ src/main/java/com/flexpag/paymentscheduler/entilty/Pagamento.java
@@ -13,10 +13,6 @@ import javax.persistence.Id;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
